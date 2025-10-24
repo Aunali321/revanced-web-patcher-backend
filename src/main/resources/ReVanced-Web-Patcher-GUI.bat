73bbf9cd1fadc4564576d61a6039760ffdd68710
Aunali321 <hello@auna.li>
2025-10-24 16:56:04 +0530

    Bundle Windows GUI into installer with .bat launcher

---
+++ src/main/resources/ReVanced-Web-Patcher-GUI.bat
@@ -0,0 +1,5 @@
+@echo off
+REM ReVanced Web Patcher - GUI Launcher
+REM This launches the PowerShell GUI
+
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0revanced-web-patcher-gui.ps1"
